e784eaff2b2abe76f94809b84ab398b6ff7eda58
Ari <aricroock@gmail.com>
2013-03-27 15:50:58 +0200

    full_title helper tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+    # returns the  ititle without a trailing | if no page title is provided
+    def full_title(page_title)
+        base_title = "Ruby on Rails Tutorial Sample App"
+        page_title.empty? ? base_title : "#{base_title} | #{page_title}"
+    end
 end
